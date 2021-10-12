110a23e8b75b8d33d8d43ce38a2d99656f719e96
Rafael Barros <rafaelbarros.df@gmail.com>
2021-10-12 15:35:12 -0300

    :construction: Service discovery Eureka e Gateway Zuul

---
+++ discovery/src/test/java/academy/devdojo/youtube/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,14 @@
+package academy.devdojo.youtube.discovery;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gateway/src/test/java/academy/devdojo/youtube/gateway/GatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package academy.devdojo.youtube.gateway;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
